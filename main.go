23964abb5f42e4b662e6d80bc919d5f249fc726a
Ваше ім'я <maroon4m@gmail.com>
2020-07-08 10:27:07 +0300

    Init comit

---
+++ main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+var tpl *template.Template
+var db *sql.DB
+
+type Lesson struct {
+	ID    int
+	Lname string
+}
+
+func init() {
+	var err error
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	connString := "dbname=gotoeleven password=password user=teacher port=5432 host=localhost sslmode=disable"
+
+	db, err = sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal("****DB not open****", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("****Ping falar****", err)
+	} else {
+		fmt.Println("Ping seccessful!!!")
+	}
+}
+
+func main() {
+	defer db.Close()
+
+	http.HandleFunc("/", hom)
+	http.HandleFunc("/add-lesson", addLesson)
+	http.HandleFunc("/set-lesson", setLesson)
+	http.HandleFunc("/update-lesson", updateLesson)
+	http.HandleFunc("/delete-lesson", deleteLesson)
+	http.Handle("/stuff/", http.StripPrefix("/stuff", http.FileServer(http.Dir("./assets/"))))
+	http.ListenAndServe(":8070", nil)
+}
+
+func hom(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("select * from lessons;")
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatal("Can`t get data", err)
+	}
+
+	ls := []Lesson{}
+	for rows.Next() {
+		l := Lesson{}
+		rows.Scan(&l.ID, &l.Lname)
+		ls = append(ls, l)
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", ls)
+}
+
+func addLesson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fn := r.FormValue("l-name")
+	fmt.Println(fn)
+
+	result, err := db.Exec("insert into lessons (Lname) values ($1);", fn)
+	if err != nil {
+		log.Fatal(err)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	fmt.Println("rows affacted", n)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func setLesson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+	id := r.FormValue("lid")
+
+	row, err := db.Query("select * from lessons where lid = $1", id)
+	defer row.Close()
+
+	fmt.Println("Selected lesson", row)
+
+	if err != nil {
+		http.Error(w, "Lesson not found", http.StatusInternalServerError)
+	}
+
+	l := Lesson{}
+
+	for row.Next() {
+		row.Scan(&l.ID, &l.Lname)
+	}
+
+	fmt.Println("l value", l)
+	tpl.ExecuteTemplate(w, "update.gohtml", l)
+}
+
+func updateLesson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	id := r.FormValue("lid")
+	fn := r.FormValue("l-name")
+	fmt.Println(fn, id)
+
+	result, err := db.Exec("update lessons set lname = $2 where lid = $1;", id, fn)
+	if err != nil {
+		log.Fatal(err)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	fmt.Println("rows affacted", n)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func deleteLesson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+	id := r.FormValue("lid")
+
+	result, err := db.Exec("delete from lessons where lid = $1;", id)
+	if err != nil {
+		http.Error(w, "Didn`t deleted", http.StatusInternalServerError)
+		return
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, "Didn`t deleted", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Rows affacted", n)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func fibgen() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	go Worker(jobs, results)
+	go Worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+}
---
+++ package-fibgen/fibgen.go
@@ -0,0 +1,36 @@
+package fibgen 
+
+import (
+	"fmt"
+)
+
+// func Fibgen() {
+// 	jobs := make(chan int, 100)
+// 	results := make(chan int, 100)
+
+// 	go worker(jobs, results)
+// 	go worker(jobs, results)
+
+// 	for i := 0; i < 100; i++ {
+// 		jobs <- i
+// 	}
+// 	close(jobs)
+
+// 	for j := 0; j < 100; j++ {
+// 		fmt.Println(<-results)
+// 	}
+// }
+
+func Worker(jobs <-chan int, results chan<- int)  {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int)  {
+	if n <=1 {
+		return n
+	}
+
+	return fib(n - 1) + fib(n - 2)
+}
